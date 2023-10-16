cebda005d8db3866360af092f88883278883ca83
Amine Bouramtane <bouramtaneamine1@gmail.com>
2023-10-16 22:48:37 +0100

    add C08

---
+++ C08/ex00/ft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abouramt <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/03 17:19:43 by abouramt          #+#    #+#             */
+/*   Updated: 2023/10/03 17:24:19 by abouramt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+
+#endif
---
+++ C08/ex04/ft_strs_to_tab.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strs_to_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abouramt <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/04 16:06:12 by abouramt          #+#    #+#             */
+/*   Updated: 2023/10/04 16:13:08 by abouramt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "ft_stock_str.h"
+
+int	ft_str_length(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(char *src)
+{
+	int		i;
+	char	*dest;
+
+	i = 0;
+	dest = (char *)malloc(ft_str_length(src) * sizeof(char) + 1);
+	if (!dest)
+		return (0);
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+struct s_stock_str	*ft_strs_to_tab(int ac, char **av)
+{
+	int					i;
+	struct s_stock_str	*array;
+
+	array = malloc((ac + 1) * sizeof(struct s_stock_str));
+	if (!array)
+		return (NULL);
+	i = 0;
+	while (i < ac)
+	{
+		array[i].size = ft_str_length(av[i]);
+		array[i].str = av[i];
+		array[i].copy = ft_strdup(av[i]);
+		i++;
+	}
+	array[i].str = 0;
+	array[i].copy = 0;
+	return (array);
+}
